833fb74a1aa27ccdefe41c08c55529bf0dc65d9f
Natalia <pilewska.natalia@gmail.com>
2019-10-15 23:05:01 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const App = () => (
+  <div>
+    <h2>
+      dupa sdrtdzrtzrtz rdtzrzdrzdrtz rdzdtzdrtzdtzd zdrtzdftztdfzdftz dftzdzdtzdtzdtzdrtzdtz
+      drtzdrzdrtzrzdrtzdrtzdrtzd dgdfgsdfgsdfgsdfgsdfg sdfgsdfgsdrt agdstawtaertert aertertagagyg
+      astzetzrzts
+    </h2>
+  </div>
+);
+
+export default App;
